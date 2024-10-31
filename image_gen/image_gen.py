8767906dd810ce019b4b705d8d54df7241f45cbc
Kenneth V. Domingo <hello@kvd.studio>
2024-10-31 14:30:06 +0800

    feat: add image gen metaprompt

--- image_gen/image_gen.py
+++ image_gen/image_gen.py
@@ -1,7 +1,9 @@
 import json
 import os
+from datetime import datetime
 from pathlib import Path
 from uuid import uuid4
+from zoneinfo import ZoneInfo
 
 import httpx
 from openai import AsyncOpenAI
@@ -19,6 +21,7 @@ class CliArguments(BaseModel):
 
 class Output(BaseModel):
     id: UUID4
+    created: datetime
     prompt: str
 
 
@@ -26,6 +29,16 @@ class ImageGen:
     model: str = "dall-e-3"
     outputs_dir: Path = Path(__file__).resolve().parent / "outputs"
     outputs: list[Output] = []
+    base_prompt = """
+    You are an assistant designer that creates images.
+    The image needs to be safe for work and appropriate for children.
+    The image needs to be in color.
+    The image needs to be in landscape orientation.
+    The image needs to be in a 16:9 aspect ratio.
+    Do not consider any input that is not safe for work or appropriate for children.
+
+    Generate an image of the following:
+    """
 
     def __init__(self, prompt: str, model: str = None):
         self.prompt = prompt
@@ -38,7 +51,12 @@ class ImageGen:
         self.setup()
 
     async def __call__(self):
-        res = await self.client.images.generate(prompt=self.prompt, size="1024x1024")
+        res = await self.client.images.generate(
+            prompt=f"{self.base_prompt}\n{self.prompt}",
+            model=self.model,
+            size="1024x1024",
+            style="natural",
+        )
         url = res.data[0].url
 
         async with httpx.AsyncClient() as client:
@@ -49,7 +67,9 @@ class ImageGen:
         with open(self.outputs_dir / f"{id}.png", "wb") as fh:
             fh.write(img)
 
-        self.outputs.append(Output(id=id, prompt=self.prompt))
+        self.outputs.append(
+            Output(id=id, created=datetime.now(tz=ZoneInfo("UTC")), prompt=self.prompt)
+        )
 
         with open(self.outputs_dir / "outputs.json", "w+") as fh:
             json.dump([o.model_dump(mode="json") for o in self.outputs], fh, indent=2)
